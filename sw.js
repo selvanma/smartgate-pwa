1626e54c24d78ff512653f3f6bd36524700cc97d
selvanma <selvanma@gmail.com>
2026-06-09 20:35:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
-const CACHE_NAME = 'smartgate-v2';
+const CACHE_NAME = 'smartgate-v4';
 const ASSETS = ['index.html', 'app.js', 'manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (e) => {
